8eb43ab6560ed5b11acbef361eeb38f1709a580a
RMKaelBR <rmbresurreccion@gmail.com>
2023-07-21 22:17:41 +0800

    + tasks now deleteable, styling for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,13 @@ class TasksController < ApplicationController
     redirect_to category_path(@category)
   end
 
+  def destroy
+    @category = Category.find(params[:category_id])
+    @task = @category.tasks.find(params[:id])
+    @task.destroy
+    redirect_to category_path(@category), status: :see_other
+  end
+  
   private
     def task_params
       params.require(:task).permit(:title, :body, :status)
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   include Visible
   
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
